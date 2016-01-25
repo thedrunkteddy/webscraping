81a46c0c7f416e064df266d3b53e4c4aa66a5406
chenfr <frankiezchen@yahoo.com>
2016-01-25 16:46:51 -0500

    Added the ratings for each book!

--- webscraping2.js
+++ webscraping2.js
@@ -33,37 +33,37 @@ if (Meteor.isClient) {
     }
 
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    var cheerio = Meteor.npmRequire('cheerio');
+    if (Meteor.isServer) {
+      Meteor.startup(function () {
+        var cheerio = Meteor.npmRequire('cheerio');
 
-    Meteor.methods({
-      webScrape: function (){
-        result = Meteor.http.get("https://www.bookbub.com/ebook-deals/latest");
-        $ = cheerio.load(result.content);
-        var titles = [];
-        var count = 0;
-        for(i=30000; i<33000; i++){
-          var resp= $('#promotion-' + i + '> div.col-sm-9 > h5 > a').text();
-          var url= "https://r.bookbub.com/promotion_site_active_check/" + i + "?retailer_id=1"
-          if (resp != "" ){
-            titles[count] = resp
-            count = count + 1;
-            Books.insert({Title: resp, url: url})
+        Meteor.methods({
+
+          webScrape: function (){
+            result = Meteor.http.get("https://www.bookbub.com/ebook-deals/latest");
+            $ = cheerio.load(result.content);
+            var titles = [];
+            var count = 0;
+            for(i=30000; i<33000; i++){
+              var resp= $('#promotion-' + i + '> div.col-sm-9 > h5 > a').text();
+
+              if (resp != "" ){
+                var url= "https://r.bookbub.com/promotion_site_active_check/" + i + "?retailer_id=1"
+                var result2 = Meteor.http.get(url);
+                $1 = cheerio.load(result2.content);
+                var resp2 = $1('#avgRating > span > a > span').text();
+                console.log(resp2)
+
+
+                titles[count] = resp + ", Rating: " + resp2
+                count = count + 1;
+                Books.insert({Title: resp, url: url})
+              }
+            }
+            var newline = titles.join("\n");
+            return newline;
+
           }
-        }
-        var newline = titles.join("\n");
-        return newline;
-
-      },
-
-      webScrape2: function (){
-        result2 = Meteor.http.get("http://www.amazon.com/The-Whiskey-Rebels-A-Novel-ebook/dp/B0015DYJVW?_bbid=1607716&tag=bookbubblog-20");
-        $ = cheerio.load(result2.content);
-        var resp2 = $('#avgRating > span > a > span').text();
-        console.log(resp2)
-        return resp2
-      }
 
     })
   });
